f95a70f35098ea503909dd46a7a5457be197af71
JulioTG <julio.torres.ga@tecsup.edu.pe>
2025-06-25 18:57:39 -0500

    Corrijo error de rutas envolviendo App con BrowserRouter

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
 <Routes>
+  <Route path="*" element={<Navigate to="/login" replace />} />
   <Route path="/login" element={<Login />} />
   <Route path="/register" element={<Register />} />
  <Route path="/home" element={
